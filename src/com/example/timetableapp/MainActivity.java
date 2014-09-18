e64bd4d9a3b7f7a767cc53ceecbb153caa9de7fc
thomaspeader <mail@peaderthomas.com>
2014-09-18 13:03:54 +1000

    Added UI layer
    Add layouts open (logo screen), day (monday tues ext), Subject
    (selected from day). And a lot of images

--- src/com/example/timetableapp/MainActivity.java
+++ src/com/example/timetableapp/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.open);
         
 //		Initialize the TextView for vertical scrolling
 		output = (TextView) findViewById(R.id.textView);
